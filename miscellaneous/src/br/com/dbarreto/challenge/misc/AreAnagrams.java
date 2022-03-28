f425d0cfeb5e297d816b6f901c474d2dd89894f3
David Barreto <david.brto@gmail.com>
2022-03-28 21:54:17 +0100

    Added hash implementation to AreAnagrams problem

--- miscellaneous/src/br/com/dbarreto/challenge/misc/AreAnagrams.java
+++ miscellaneous/src/br/com/dbarreto/challenge/misc/AreAnagrams.java
@@ -1,13 +1,44 @@
 package br.com.dbarreto.challenge.misc;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AreAnagrams {
 
 	public boolean solveUsingHash(String s1, String s2) {
 		
+		if (s1 == null && s2 == null) {
+			return true;
+		}
 		
-		return false;
+		if ((s1 != null && s2 == null) || (s1 == null && s2 != null)) {
+			return false;
+		}
+		
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		
+		Map<Integer, Integer> freqs = new HashMap<>();
+		addFreq(s1, freqs);
+		decreaseFreq(s2, freqs);
+		
+		return freqs.values().stream().filter(a -> a != 0).count() == 0;
+	}
+	
+	private void addFreq(String s, Map<Integer, Integer> freqs) {
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			freqs.merge((int)c, 1, Integer::sum);
+		}
+	}
+	
+	private void decreaseFreq(String s, Map<Integer, Integer> freqs) {
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			freqs.merge((int)c, 0, (a, b) -> a - 1);
+		}
 	}
 	
 	/**
@@ -19,6 +50,18 @@ public class AreAnagrams {
 	 * It's bad if there are a large set of possible characters
 	 */
 	public boolean solveUsingArray(String s1, String s2) {
+		
+		if (s1 == null && s2 == null) {
+			return true;
+		}
+		
+		if ((s1 != null && s2 == null) || (s1 == null && s2 != null)) {
+			return false;
+		}
+		
+		if (s1.length() != s2.length()) {
+			return false;
+		}
 		
 		short[] freq1 = new short[26];
 		short[] freq2 = new short[26];
---
+++ miscellaneous/test/br/com/dbarreto/challenge/misc/test/TestAreAnagrams.java
@@ -0,0 +1,33 @@
+package br.com.dbarreto.challenge.misc.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.dbarreto.challenge.misc.AreAnagrams;
+
+public class TestAreAnagrams {
+	
+	private AreAnagrams problem = new AreAnagrams();
+
+	@Test
+	public void testHashSolutionAnagram() {
+		assertTrue(problem.solveUsingHash("maca", "cama"));
+	}
+	
+	@Test
+	public void testHashSolutionNotAnagram() {
+		assertFalse(problem.solveUsingHash("maca", "abcd"));
+	}
+	
+	@Test
+	public void testArraySolutionAnagram() {
+		assertTrue(problem.solveUsingArray("maca", "cama"));
+	}
+	
+	@Test
+	public void testArraySolutionNotAnagram() {
+		assertFalse(problem.solveUsingArray("maca", "abcd"));
+	}
+}
