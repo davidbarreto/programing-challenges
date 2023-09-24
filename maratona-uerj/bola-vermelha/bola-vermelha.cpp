944d80d14c53ea50cc7d677bb6202ad8944ef112
David Barreto <david.brto@gmail.com>
2023-09-24 18:34:20 +0100

    Refactor solutions to encapsulate code in methods

--- maratona-uerj/bola-vermelha/bola-vermelha.cpp
+++ maratona-uerj/bola-vermelha/bola-vermelha.cpp
@@ -8,11 +8,12 @@ using namespace std;
 
 bool livingRoom[MAX_N][MAX_N];
 bool visited[MAX_N][MAX_N];
-queue<tuple<int, int, int>> pathQueue;
 
-bool isValid(int x, int y, int n);
-void enqueueCoord(int x, int y, int dist, int n);
-void resetVisited(int n);
+bool isValid(int, int, int);
+void enqueueCoord(queue<tuple<int, int, int>>&, int, int, int, int);
+void reset(int);
+int calculateDistance(int, int, int, int, int);
+void readMatrix(int);
 
 int main() {
 
@@ -24,52 +25,49 @@ int main() {
         int n;
         cin >> n;
 
-        resetVisited(n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> livingRoom[i][j]; 
-            }
-        }
+        reset(n);
+        readMatrix(n);
 
         int startX, startY, endX, endY;
         cin >> startX >> startY >> endX >> endY;
 
-        pathQueue.push(make_tuple(startX, startY, 0));
-
-        bool foundResult = false;
-        while (!pathQueue.empty()) {
-            tuple<int, int, int> coord = pathQueue.front();
-            
-            int x = get<0>(coord);
-            int y = get<1>(coord);
-            int dist = get<2>(coord);
-
-            pathQueue.pop();
-
-            if (x == endX && y == endY) {
-                cout << dist << endl;
-                foundResult = true;
-                break;
-            }
-
-            int nextDist = dist + 1;
-            enqueueCoord(x+1, y, nextDist, n);
-            enqueueCoord(x-1, y, nextDist, n);
-            enqueueCoord(x, y+1, nextDist, n);
-            enqueueCoord(x, y-1, nextDist, n);
-        }
-
-        if (!foundResult) {
-            cout << -1 << endl;
-        }
-        
+        cout << calculateDistance(startX, startY, endX, endY, n) << endl;
     }
  
     return 0;
 }
 
-void enqueueCoord(int x, int y, int dist, int n) {
+int calculateDistance(int startX, int startY, int endX, int endY, int n) {
+
+    queue<tuple<int, int, int>> pathQueue;
+    pathQueue.push(make_tuple(startX, startY, 0));
+
+    int result = -1;
+    while (!pathQueue.empty()) {
+        tuple<int, int, int> coord = pathQueue.front();
+        
+        int x = get<0>(coord);
+        int y = get<1>(coord);
+        int dist = get<2>(coord);
+
+        pathQueue.pop();
+
+        if (x == endX && y == endY) {
+            result = dist;
+            break;
+        }
+
+        int nextDist = dist + 1;
+        enqueueCoord(pathQueue, x+1, y, nextDist, n);
+        enqueueCoord(pathQueue, x-1, y, nextDist, n);
+        enqueueCoord(pathQueue, x, y+1, nextDist, n);
+        enqueueCoord(pathQueue, x, y-1, nextDist, n);
+    }
+
+    return result;
+}
+
+void enqueueCoord(queue<tuple<int, int, int>>& pathQueue, int x, int y, int dist, int n) {
 
     if (isValid(x, y, n)) {
         pathQueue.push(make_tuple(x, y, dist));
@@ -81,10 +79,18 @@ bool isValid(int x, int y, int n) {
     return x >= 0 && x < n && y >= 0 && y < n && !visited[x][y] && !livingRoom[x][y];
 }
 
-void resetVisited(int n) {
+void reset(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             visited[i][j] = false;
+        }
+    }
+}
+
+void readMatrix(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> livingRoom[i][j]; 
         }
     }
 }
